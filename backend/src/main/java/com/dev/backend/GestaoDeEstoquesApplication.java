818ee80a62aeec54899196bec205f0dfa3eb3f69
DanielDev-java <manodaniel429@gmail.com>
2025-02-13 20:44:19 -0300

    Criacao Backend

---
+++ backend/src/main/java/com/dev/backend/GestaoDeEstoquesApplication.java
@@ -0,0 +1,14 @@
+package com.dev.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeEstoquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeEstoquesApplication.class, args);
+        System.out.println("API RODANDO....");
+	}
+
+}
