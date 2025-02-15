314424b1ff8ed5a6ef45d413fa5c9091f119336a
DanielDev-java <manodaniel429@gmail.com>
2025-02-15 17:40:08 -0300

    Seed bd 2

--- backend/src/main/java/com/dev/backend/configs/TestConfig.java
+++ backend/src/main/java/com/dev/backend/configs/TestConfig.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.dev.backend.entities.Category;
 import com.dev.backend.entities.Product;
+import com.dev.backend.repositories.CategoryRepository;
 import com.dev.backend.repositories.ProductRepository;
 
 @Configuration
@@ -15,11 +17,19 @@ import com.dev.backend.repositories.ProductRepository;
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
         Product p1 = new Product(null, "Café", "Café Brasileiro", "g", 8.50, 17.00, 5, 85, 30,
                 LocalDate.parse("2025-06-20"));
+        productRepository.save(p1);
+
+        Category c1 = new Category(null, "Café", "Category Cafe");
+        categoryRepository.save(c1);
+
+        p1.getCategories().add(c1);
         productRepository.save(p1);
     }
 
